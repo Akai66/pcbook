a2fba3d3eaae98709d20ff4dbb45914238abd878
liukai <869896679@qq.com>
2021-09-27 20:53:22 +0800

    服务端流模式-添加测试代码

--- service/laptop_client_test.go
+++ service/laptop_client_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"io"
 	"net"
 	"pcbook/pb"
 	"pcbook/sample"
@@ -15,7 +16,7 @@ import (
 func TestLaptopClient_CreateLaptop(t *testing.T) {
 	t.Parallel()
 
-	laptopServer, serverAddr := startTestLaptopServer(t)
+	laptopServer, serverAddr := startTestLaptopServer(t, NewInMemoryLaptopStore())
 	laptopClient := newTestLaptopClient(t, serverAddr)
 
 	laptop := sample.NewLaptop()
@@ -42,9 +43,73 @@ func TestLaptopClient_CreateLaptop(t *testing.T) {
 	requireSameLaptop(t, laptop, other)
 }
 
+func TestLaptopClient_SearchLaptop(t *testing.T) {
+	t.Parallel()
+
+	filter := &pb.Filter{
+		MaxPriceUsed: 3000,
+		MinCpuCores:  4,
+		MinCpuGhz:    2.5,
+		MinRam:       &pb.Memory{Value: 8, Unit: pb.Memory_GIGABYTE},
+	}
+	store := NewInMemoryLaptopStore()
+	expectedIDs := make(map[string]bool)
+
+	for i := 0; i < 6; i++ {
+		laptop := sample.NewLaptop()
+		switch i {
+		case 0:
+			laptop.PriceUsed = 3100
+		case 1:
+			laptop.Cpu.NumberCores = 2
+		case 2:
+			laptop.Cpu.MinGhz = 2.4
+		case 3:
+			laptop.Ram = &pb.Memory{Value: 4096, Unit: pb.Memory_MEGABYTE}
+		case 4:
+			laptop.PriceUsed = 2999.99
+			laptop.Cpu.NumberCores = 6
+			laptop.Cpu.MinGhz = 2.6
+			laptop.Ram = &pb.Memory{Value: 9, Unit: pb.Memory_GIGABYTE}
+			expectedIDs[laptop.Id] = true
+		case 5:
+			laptop.PriceUsed = 2800
+			laptop.Cpu.NumberCores = 8
+			laptop.Cpu.MinGhz = 3.1
+			laptop.Ram = &pb.Memory{Value: 10, Unit: pb.Memory_GIGABYTE}
+			expectedIDs[laptop.Id] = true
+		}
+		err := store.Save(laptop)
+		require.NoError(t, err)
+	}
+
+	_, serverAddr := startTestLaptopServer(t, store)
+	laptopClient := newTestLaptopClient(t, serverAddr)
+	req := &pb.SearchLaptopRequest{
+		Filter: filter,
+	}
+	stream, err := laptopClient.SearchLaptop(context.Background(), req)
+	require.NoError(t, err)
+	count := 0
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+		require.NotNil(t, res)
+		require.Contains(t, expectedIDs, res.GetLaptop().GetId())
+
+		count++
+	}
+
+	require.Equal(t, len(expectedIDs), count)
+}
+
 // startTestLaptopServer 启动一个测试的grpc服务器
-func startTestLaptopServer(t *testing.T) (*LaptopServer, string) {
-	laptopServer := NewLaptopServer(NewInMemoryLaptopStore())
+func startTestLaptopServer(t *testing.T, store LaptopStore) (*LaptopServer, string) {
+	laptopServer := NewLaptopServer(store)
 	grpcServer := grpc.NewServer()
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
 	listener, err := net.Listen("tcp", ":0") //随机监听一个可用的端口
@@ -64,7 +129,7 @@ func newTestLaptopClient(t *testing.T, serverAddr string) pb.LaptopServiceClient
 
 func requireSameLaptop(t *testing.T, laptop1, laptop2 *pb.Laptop) {
 	//这里二者是不相等的，因为在结构体内部有一些特殊领域，用于序列化对象使用
-	require.Equal(t, laptop1, laptop2)
+	//require.Equal(t, laptop1, laptop2)
 	//通过proto.Equal()方法比较，二者是相等的
 	require.True(t, proto.Equal(laptop1, laptop2))
 	//通过将proto对象转换为json字符串比较，也是相等的
