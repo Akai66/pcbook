672c5f0f9cf83e30d5740426112c5d23b98a8106
liukai <869896679@qq.com>
2021-09-27 16:21:41 +0800

    创建正式的client和server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,18 +2,38 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"flag"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
 	"pcbook/pb"
 	"pcbook/sample"
 )
 
 func main() {
-	conn, _ := grpc.Dial(":8080", grpc.WithInsecure())
+	serverAddr := flag.String("address", "", "rpc server address")
+	flag.Parse()
+	log.Printf("dial server %s", *serverAddr)
+
+	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("cannot dial server: %v", err)
+	}
 	laptopClient := pb.NewLaptopServiceClient(conn)
 	req := &pb.CreateLaptopRequest{
 		Laptop: sample.NewLaptop(),
 	}
-	res, _ := laptopClient.CreateLaptop(context.Background(), req)
-	fmt.Println(res)
+	res, err := laptopClient.CreateLaptop(context.Background(), req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.AlreadyExists {
+			log.Print("laptop already exists")
+		} else {
+			log.Fatalf("cannot create laptop: %v", err)
+		}
+		return
+	}
+
+	log.Printf("created laptop with id: %s", res.Id)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"google.golang.org/grpc"
+	"log"
 	"net"
 	"pcbook/pb"
 	"pcbook/service"
 )
 
 func main() {
+	port := flag.Int("port", 0, "rpc server port")
+	flag.Parse()
+	log.Printf("start server on port: %d", *port)
 	laptopServer := service.NewLaptopServer(service.NewInMemoryLaptopStore())
 	grpcServer := grpc.NewServer()
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
-	listener, _ := net.Listen("tcp", ":8080") //随机监听一个可用的端口
-	grpcServer.Serve(listener)
+
+	address := fmt.Sprintf("0.0.0.0:%d", *port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("cannot start server: %v", err)
+	}
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("cannot start server: %v", err)
+	}
 }
